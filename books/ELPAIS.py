659258d6fbb52b9cb4ef4a8176e3f372ceafd6fd
kimfyy <50686481+kimfyy@users.noreply.github.com>
2019-05-16 22:50:05 +0800

    Update ELPAIS.py

--- books/ELPAIS.py
+++ books/ELPAIS.py
@@ -7,27 +7,27 @@ def getBook():
     return Elpais
 
 class Elpais(BaseFeedBook):
-    title                 = 'El País'
-    description           = '西班牙《国家报》Noticias de última hora sobre la actualidad en España y el mundo.'
+    title                 = u'El País'
+    description           = u'西班牙《国家报》Noticias de última hora sobre la actualidad en España y el mundo.'
     language              = 'es'
     feed_encoding         = "utf-8"
     page_encoding         = "utf-8"
     mastheadfile          = "Elpais.gif"
     coverfile             = "cover-es.jpg"
     feeds = [
-        ('Lo último', 'http://ep00.epimg.net/rss/tags/ultimas_noticias.xml'),
-        ('Lo más visto', 'http://ep00.epimg.net/rss/tags/noticias_mas_vistas.xml'),
-        ('INTERNACIONAL', 'http://ep00.epimg.net/rss/internacional/portada.xml'),
-        ('OPINIÓN', 'http://ep01.epimg.net/rss/elpais/opinion.xml'),
-        ('ESPAÑA', 'http://ep00.epimg.net/rss/politica/portada.xml'),
-        ('ECONOMÍA', 'http://ep00.epimg.net/rss/economia/portada.xml'),
-        ('TECNOLOGÍA', 'http://ep01.epimg.net/rss/tecnologia/portada.xml'),
-        ('CIENCIA', 'http://ep00.epimg.net/rss/elpais/ciencia.xml'),
-        ('ESTILO', 'http://ep00.epimg.net/rss/elpais/estilo.xml'),
-        ('DEPORTES', 'http://ep00.epimg.net/rss/deportes/portada.xml'),
-        ('CULTURA', 'http://ep00.epimg.net/rss/cultura/television.xml'),
-        ('SOCIEDAD', 'http://ep00.epimg.net/rss/sociedad/portada.xml'),
-        ('DEPORTES', 'http://ep00.epimg.net/rss/deportes/portada.xml'),
-        ]    
+            (u'Lo último', 'http://ep00.epimg.net/rss/tags/ultimas_noticias.xml'),
+            (u'Lo más visto', 'http://ep00.epimg.net/rss/tags/noticias_mas_vistas.xml'),
+            (u'INTERNACIONAL', 'http://ep00.epimg.net/rss/internacional/portada.xml'),
+            (u'OPINIÓN', 'http://ep01.epimg.net/rss/elpais/opinion.xml'),
+            (u'ESPAÑA', 'http://ep00.epimg.net/rss/politica/portada.xml'),
+            (u'ECONOMÍA', 'http://ep00.epimg.net/rss/economia/portada.xml'),
+            (u'TECNOLOGÍA', 'http://ep01.epimg.net/rss/tecnologia/portada.xml'),
+            (u'CIENCIA', 'http://ep00.epimg.net/rss/elpais/ciencia.xml'),
+            (u'ESTILO', 'http://ep00.epimg.net/rss/elpais/estilo.xml'),
+            (u'DEPORTES', 'http://ep00.epimg.net/rss/deportes/portada.xml'),
+            (u'CULTURA', 'http://ep00.epimg.net/rss/cultura/television.xml'),
+            (u'SOCIEDAD', 'http://ep00.epimg.net/rss/sociedad/portada.xml'),
+            (u'DEPORTES', 'http://ep00.epimg.net/rss/deportes/portada.xml'),
+            ]    
     
     
